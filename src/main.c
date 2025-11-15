dc07fcf81e3e80b0234cfbe884aac70cf2bd69b0
Solractys <carloseduardosilvasouzadelima@gmail.com>
2025-11-15 19:26:38 -0300

    runing a simple window

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include "../includes/ft_printf/ft_printf.h"
 #include "../includes/map.h"
 
+
 int main(void)
 {
     void    *mlx;
@@ -14,24 +15,14 @@ int main(void)
         return (1);
     }
     win = mlx_new_window(mlx, 100, 100, "Hello, World!");
-    if (!win)
+        if (!win)
     {
         ft_printf("Error: mlx_new_window failed\n");
-        mlx_destroy_display(mlx);
-        free(mlx);
         return (1);
     }
     ft_printf("Window created successfully!\n");
     t_map *map;
     map = read_map("maps/map.ber");
-    if (!map)
-    {
-        ft_printf("Error: read_map failed\n");
-        mlx_destroy_window(mlx, win);
-        mlx_destroy_display(mlx);
-        free(mlx);
-        return (1);
-    }
     int x;
     int y;
     y = 0;
