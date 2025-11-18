d516e63d6ee8c59bdf9cb141a72a35d6428c17a0
Solractys <carloseduardosilvasouzadelima@gmail.com>
2025-11-18 00:39:29 -0300

    render map

--- src/render/load_images.c
+++ src/render/load_images.c
@@ -2,9 +2,14 @@
 
 void load_images(t_game *game)
 {
-    game->wall_img = mlx_xpm_file_to_image(game->mlx, "assets/wall.xpm", 32, 32);
-    game->floor_img = mlx_xpm_file_to_image(game->mlx, "assets/floor.xpm", 32, 32);
-    game->player_img = mlx_xpm_file_to_image(game->mlx, "assets/player.xpm", 32, 32);
-    game->exit_img = mlx_xpm_file_to_image(game->mlx, "assets/exit.xpm", 32, 32);
-    game->collectible_img = mlx_xpm_file_to_image(game->mlx, "assets/collectible.xpm", 32, 32);
+    int width;
+    int height;
+
+    width = 32;
+    height = 32;
+    game->wall_img = mlx_xpm_file_to_image(game->mlx, "src/assets/wall.xpm", &width, &height);
+    game->floor_img = mlx_xpm_file_to_image(game->mlx, "src/assets/floor.xpm", &width, &height);
+    game->player_img = mlx_xpm_file_to_image(game->mlx, "src/assets/floor.xpm", &width, &height);
+    game->exit_img = mlx_xpm_file_to_image(game->mlx, "src/assets/exit.xpm", &width, &height);
+    game->collectible_img = mlx_xpm_file_to_image(game->mlx, "src/assets/collectible.xpm", &width, &height);
 }
